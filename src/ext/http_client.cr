2d2e95a743ac401bde3f75f299552111838c775f
Anton Maminov <anton.linux@gmail.com>
2022-10-03 19:58:01 +0300

    fix internal implementation for Crystal >= 1.6.0

--- src/ext/http_client.cr
+++ src/ext/http_client.cr
@@ -2,23 +2,41 @@ require "http/client"
 require "digest"
 
 class HTTP::Client
-  private def orig_exec_internal_single(request)
-    decompress = send_request(request)
-    HTTP::Client::Response.from_io?(io, ignore_body: request.ignore_body?, decompress: decompress)
-  end
-
-  private def exec_internal_single(request)
-    cassette_name = VCR.cassette_name
-
-    # If we do not have a cassette_name, perform a normal request
-    if (cassette_name.nil?)
-      orig_exec_internal_single(request)
-    else
-      _vcr_record(request)
+  {% if compare_versions(Crystal::VERSION, "1.6.0-0") >= 0 %}
+    private def orig_exec_internal_single(request, implicit_compression = false)
+      send_request(request)
+      HTTP::Client::Response.from_io?(io, ignore_body: request.ignore_body?, decompress: implicit_compression)
     end
-  end
 
-  private def _vcr_record(request)
+    private def exec_internal_single(request, implicit_compression = false)
+      cassette_name = VCR.cassette_name
+
+      # If we do not have a cassette_name, perform a normal request
+      if (cassette_name.nil?)
+        orig_exec_internal_single(request, implicit_compression: implicit_compression)
+      else
+        _vcr_record(request, implicit_compression: implicit_compression)
+      end
+    end
+  {% else %}
+    private def orig_exec_internal_single(request)
+      decompress = send_request(request)
+      HTTP::Client::Response.from_io?(io, ignore_body: request.ignore_body?, decompress: decompress)
+    end
+
+    private def exec_internal_single(request)
+      cassette_name = VCR.cassette_name
+
+      # If we do not have a cassette_name, perform a normal request
+      if (cassette_name.nil?)
+        orig_exec_internal_single(request)
+      else
+        _vcr_record(request)
+      end
+    end
+  {% end %}
+
+  private def _vcr_record(request, implicit_compression = false)
     # Create an md5 for the request
     secured_request = VCR.filter_sensitive_data!(request)
     req_md5 = Digest::MD5.hexdigest(secured_request.to_json)
@@ -41,7 +59,12 @@ class HTTP::Client
       end
       response
     else
-      response = orig_exec_internal_single(request)
+      {% if compare_versions(Crystal::VERSION, "1.6.0-0") >= 0 %}
+        response = orig_exec_internal_single(request, implicit_compression: implicit_compression)
+      {% else %}
+        response = orig_exec_internal_single(request)
+      {% end %}
+
       unless response.nil?
         io = IO::Memory.new
         response.to_io(io)
